f7d04b4d1f1c4f3ccaeb847986388e8ac5177745
Christina <christinachon90@gmail.com>
2019-07-31 18:03:50 -0700

    added spotify search

--- liri.js
+++ liri.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var moment = require('moment');
 
-//var spotify = new Spotify(keys, spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 var action = process.argv[2];
 
@@ -21,4 +22,23 @@ if (action === "concert-this") {
         }
 
     })
+}
+
+if (action === "spotify-this-song"){
+    var song = process.argv[3];
+
+    spotify.search({ type: 'track', query: song, limit:5}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+        for (var i=0; i < data.tracks.items.length; i++){
+            console.log("Artist: " + data.tracks.items[i].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[i].name);
+            console.log("Album Name: " + data.tracks.items[i].album.name);
+            console.log("Spotify Link: " + data.tracks.items[i].external_urls.spotify);
+            console.log("----")
+        }
+       
+      });
 }
